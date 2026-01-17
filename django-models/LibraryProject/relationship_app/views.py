d8085a8ff6dc8db17eb5158447ef1edb0bb61002
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-17 23:48:13 +0200

    Ignore Python cache files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Book, Library
+from .models import Library
 
 
-# Function-based view: list all books
 def list_books(request):
+    from .models import Book
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
 
 
-# Class-based view: library detail
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
