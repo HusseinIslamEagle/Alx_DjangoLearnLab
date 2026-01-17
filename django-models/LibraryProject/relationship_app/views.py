4b9d161c17cae87c38dfc01f37cea585bf277212
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-17 23:56:17 +0200

    Use django.contrib.auth login in authentication views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 
 
-# Login view (built-in)
 class UserLoginView(LoginView):
     template_name = 'relationship_app/login.html'
 
 
-# Logout view (built-in)
 class UserLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
 
 
-# Registration view
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('login')
     else:
         form = UserCreationForm()
