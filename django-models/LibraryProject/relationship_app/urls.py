9da53ebe48dc40743b987b2abc0aa7176304a9fb
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-18 00:34:17 +0200

    Restore UserProfile model alongside Book custom permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView
 from . import views
 
 urlpatterns = [
+    # Function-based view
+    path('books/', list_books, name='list_books'),
+
+    # Class-based view
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
     # Home
     path('', views.home, name='home'),
 
