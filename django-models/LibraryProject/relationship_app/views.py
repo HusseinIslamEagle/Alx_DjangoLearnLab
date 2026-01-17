0c7c06c32a8cffd0391b7c0d3bb5462029a77dd9
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-17 23:54:56 +0200

    Ignore Python cache files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-
-from .models import Library
-
-
-def list_books(request):
-    from .models import Book
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {
-        'books': books
-    })
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Login view (built-in)
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+# Logout view (built-in)
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
