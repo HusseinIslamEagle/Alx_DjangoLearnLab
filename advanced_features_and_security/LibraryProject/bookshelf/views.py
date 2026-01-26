132c3acc7da807bb3e7f213f746a55da013eb1d4
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-26 17:33:26 +0200

    Apply Django security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
-from .forms import BookSearchForm
+from .forms import ExampleForm
 
 
 def book_list(request):
-    form = BookSearchForm(request.GET or None)
+    form = ExampleForm(request.GET or None)
     books = Book.objects.all()
 
     if form.is_valid():
-        query = form.cleaned_data['query']
-        books = Book.objects.filter(title__icontains=query)
+        query = form.cleaned_data.get('title')
+        if query:
+            books = Book.objects.filter(title__icontains=query)
 
     return render(request, 'bookshelf/book_list.html', {
         'form': form,
