6c3fa53d8abc3261be7ad994c360d17ef5aeef86
Hussein Ahmed <aha.hussein3@gmail.com>
2026-01-26 17:31:45 +0200

    Apply Django security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
-class BookSearchForm(forms.Form):
-    query = forms.CharField(max_length=100)
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
