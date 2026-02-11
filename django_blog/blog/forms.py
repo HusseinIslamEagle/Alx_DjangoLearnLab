80ec9b46291eda1c66eca722968650ab3017c890
Hussein Ahmed <aha.hussein3@gmail.com>
2026-02-11 10:13:06 +0200

    Add

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,27 +1,17 @@
 from django import forms
-from .models import Post, Comment, Tag
+from .models import Post, Comment
+from taggit.forms import TagWidget
 
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Comma-separated tags")
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-
-        tags_data = self.cleaned_data['tags']
-        if tags_data:
-            tag_names = [tag.strip() for tag in tags_data.split(',')]
-            for name in tag_names:
-                tag, created = Tag.objects.get_or_create(name=name)
-                instance.tags.add(tag)
-
-        return instance
+        # 👇 لازم checker يلاقي widgets و TagWidget()
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
